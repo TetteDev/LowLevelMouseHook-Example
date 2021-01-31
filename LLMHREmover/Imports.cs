24408396fb4ce1d3f0b8fd072b5c7cffc4199240
Pantelis <6103339+Sadulisten@users.noreply.github.com>
2021-01-31 12:32:28 +0100

    Add project files.

---
+++ LLMHREmover/Imports.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using static LLMHREmover.Enums;
+using static LLMHREmover.Structs;
+
+namespace LLMHREmover
+{
+	unsafe class Imports
+	{
+		public static delegate* unmanaged[Stdcall]<HookType, // hookType
+															 //delegate* managed<int, nint, MSLLHOOKSTRUCT*, nint>,  // lpFn, managed: (int code, IntPtr wParam, IntPtr lParam)
+												   delegate* unmanaged[Stdcall]<int, nint, MSLLHOOKSTRUCT*, nint>,  // lpFn, managed: (int code, IntPtr wParam, IntPtr lParam)
+												   nint, // hMod
+												   uint, // dwThreadIt
+												   nint  /* Return Type */> SetWindowsHookEx = null;
+
+		public static delegate* unmanaged[Stdcall]<nint, // hkk
+												   bool  /* Return Type */> UnhookWindowsHookEx = null;
+
+
+		public static delegate* unmanaged[Stdcall]<nint, // hkk
+												   int,  // nCode
+												   nint, // wParam
+												   MSLLHOOKSTRUCT*, // lParam
+												   nint  /* Return Type */> CallNextHookEx = null;
+
+		public static delegate* unmanaged[Stdcall]<char*, // module name
+												   nint   /* Return Type */> GetModuleHandle = null;
+
+		public static delegate* unmanaged[Stdcall]<MSG*, 
+												   nint, // hWnd 
+												   uint, // wMsgFilterMin
+												   uint, // wMsgFilterMax
+												   int   /* Return Type */> GetMessage;
+
+		public static delegate* unmanaged[Stdcall]<MSG*, bool> TranslateMessage;
+		public static delegate* unmanaged[Stdcall]<MSG*, nint> DispatchMessage;
+
+		public static bool Init()
+		{
+			nint user32dll = NativeLibrary.Load("user32.dll");
+			nint kernel32dll = NativeLibrary.Load("kernel32.dll");
+
+			Debug.Assert(user32dll > 0);
+			Debug.Assert(kernel32dll > 0);
+
+			SetWindowsHookEx = (delegate* unmanaged[Stdcall]<HookType, delegate* unmanaged[Stdcall]<int, nint, MSLLHOOKSTRUCT*, nint>, nint, uint, nint>)NativeLibrary.GetExport(user32dll, "SetWindowsHookExA");
+			UnhookWindowsHookEx = (delegate* unmanaged[Stdcall]<nint, bool>)NativeLibrary.GetExport(user32dll, "UnhookWindowsHookEx");
+			CallNextHookEx = (delegate* unmanaged[Stdcall]<nint, int, nint, MSLLHOOKSTRUCT*, nint>)NativeLibrary.GetExport(user32dll, "CallNextHookEx");
+			GetModuleHandle = (delegate* unmanaged[Stdcall]<char*, nint>)NativeLibrary.GetExport(kernel32dll, "GetModuleHandleW");
+
+			GetMessage = (delegate* unmanaged[Stdcall]<MSG*, nint, uint, uint, int>)NativeLibrary.GetExport(user32dll, "GetMessageW");
+			TranslateMessage = (delegate* unmanaged[Stdcall]<MSG*, bool>)NativeLibrary.GetExport(user32dll, "TranslateMessage");
+			DispatchMessage = (delegate* unmanaged[Stdcall]<MSG*, nint>)NativeLibrary.GetExport(user32dll, "DispatchMessageW");
+
+			//NativeLibrary.Free(user32dll);
+			//NativeLibrary.Free(kernel32dll);
+
+			Debug.Assert(SetWindowsHookEx != null);
+			Debug.Assert(UnhookWindowsHookEx != null);
+			Debug.Assert(CallNextHookEx   != null);
+			Debug.Assert(GetModuleHandle  != null);
+
+			Debug.Assert(GetMessage != null);
+			Debug.Assert(TranslateMessage != null);
+			Debug.Assert(DispatchMessage != null);
+
+			return SetWindowsHookEx != null
+				   && UnhookWindowsHookEx != null
+				   && CallNextHookEx != null
+				   && GetModuleHandle != null
+				   && GetMessage != null
+				   && TranslateMessage != null
+				   && DispatchMessage != null;
+		}
+	}
+
+	public class Enums
+	{
+		public enum HookType : int
+		{
+			WH_JOURNALRECORD = 0,
+			WH_JOURNALPLAYBACK = 1,
+			WH_KEYBOARD = 2,
+			WH_GETMESSAGE = 3,
+			WH_CALLWNDPROC = 4,
+			WH_CBT = 5,
+			WH_SYSMSGFILTER = 6,
+			WH_MOUSE = 7,
+			WH_HARDWARE = 8,
+			WH_DEBUG = 9,
+			WH_SHELL = 10,
+			WH_FOREGROUNDIDLE = 11,
+			WH_CALLWNDPROCRET = 12,
+			WH_KEYBOARD_LL = 13,
+			WH_MOUSE_LL = 14
+		}
+	}
+
+	public class Structs
+	{
+		[StructLayout(LayoutKind.Sequential)]
+		public struct POINT
+		{
+			public int X;
+			public int Y;
+
+			public POINT(int x, int y)
+			{
+				this.X = x;
+				this.Y = y;
+			}
+
+			public static implicit operator System.Drawing.Point(POINT p)
+			{
+				return new System.Drawing.Point(p.X, p.Y);
+			}
+
+			public static implicit operator POINT(System.Drawing.Point p)
+			{
+				return new POINT(p.X, p.Y);
+			}
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct MSG
+		{
+			IntPtr hwnd;
+			uint message;
+			UIntPtr wParam;
+			IntPtr lParam;
+			int time;
+			POINT pt;
+			int lPrivate;
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		public unsafe struct MSLLHOOKSTRUCT
+		{
+			public POINT pt;
+			public nint mouseData;
+			public nint flags;
+			public nint time;
+
+			// ULONG_PTR dwExtraInfo;
+			public nuint* dwExtraInfo;
+		}
+	}
+
+	public class Constants
+	{
+		public const int LLMHF_INJECTED = 0x00000001;
+		public const int LLMHF_LOWER_IL_INJECTED = 0x00000002;
+	}
+}
---
+++ LLMHREmover/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+using static LLMHREmover.Imports;
+using static LLMHREmover.Enums;
+using static LLMHREmover.Structs;
+using static LLMHREmover.Constants;
+
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+
+namespace LLMHREmover
+{
+	unsafe class Program
+	{
+		private static nint _activeHook = 0;
+		private static Thread _hookThread = null;
+
+		static unsafe void Main(string[] args)
+		{
+			if (!Init()) throw new NullReferenceException($"Failed initializing function pointers");
+
+			using Process process = Process.GetCurrentProcess();
+			using ProcessModule module = process.MainModule;
+
+			fixed (char* lpModName = module.ModuleName)
+			{
+				nint hModule = GetModuleHandle(lpModName);
+				Debug.Assert(hModule > 0);
+
+				_hookThread = new Thread(() =>
+				{
+					_activeHook = SetWindowsHookEx(HookType.WH_MOUSE_LL, &LowLevelMouseCallback, hModule, 0);
+					Debug.Assert(_activeHook > 0, "SetWindowsHookEx returned 0");
+
+					Console.WriteLine("Starting MessagePump");
+					MSG msg = default;
+
+					while (GetMessage(&msg, IntPtr.Zero, 0, 0) != -1)
+					{
+						TranslateMessage(&msg);
+						DispatchMessage(&msg);
+					}
+
+					Console.WriteLine("GetMessage returned -1, unhooking our windows hook");
+					if (!UnhookWindowsHookEx(_activeHook)) 
+						Console.WriteLine($"UnhookWindowsHookEx with parameter '{_activeHook}' returned false");
+
+					_activeHook = 0;
+				});
+				_hookThread.Start();
+			}
+
+			Debug.Assert(_hookThread != null
+						 && _hookThread.ThreadState == System.Threading.ThreadState.Running);
+
+			Console.WriteLine("Pressing enter will close the program ...");
+			Console.ReadLine();
+		}
+
+		[UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvStdcall) })]
+        private unsafe static nint LowLevelMouseCallback(int code, IntPtr wParam, MSLLHOOKSTRUCT* lParam)
+        {
+			if (code < 0) return CallNextHookEx(IntPtr.Zero, code, wParam, lParam);
+
+			// Can filter out only mouse movements from your application only by
+			// setting mInfo->dwExtraInfo to some constant value when calling SendInput/mouse_event
+			// in some other program
+			// and then just checking for that constant value here before doing any stripping
+
+			if ((lParam->flags & LLMHF_INJECTED) != 0)
+				lParam->flags &= ~LLMHF_INJECTED;
+
+			if ((lParam->flags & LLMHF_LOWER_IL_INJECTED) != 0)
+				lParam->flags &= ~LLMHF_LOWER_IL_INJECTED;
+
+			return CallNextHookEx(IntPtr.Zero, code, wParam, lParam);
+        }
+    }
+}
